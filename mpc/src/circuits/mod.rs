493f994533561031523c579ec596a00581704026
teapotd <teapot404@gmail.com>
2022-01-29 13:30:01 +0100

    Fix join_circuits hygienie

--- mpc/src/circuits/mod.rs
+++ mpc/src/circuits/mod.rs
@@ -1,14 +1,16 @@
 pub mod elementary;
 
+pub use futures; // Reexport futures crate for join_circuits! macro.
+
 /// Wait on multiple concurrent branches, returning when **all** branches complete.
-/// This macro guarantees deterministic polling order of provided futurs,
-/// which makes it safe to use with our circuits.
+/// This macro guarantees deterministic polling order of provided futures,
+/// which makes it safe to use with our async circuits.
 #[macro_export]
 #[cfg_attr(docsrs, doc(cfg(feature = "macros")))]
 macro_rules! join_circuits {
     // TODO: Currently we rely on the fact that futures::join! polls futures in the same order each time.
     // This is undocumented, so we shouldn't rely on this.
     ($($tokens:tt)*) => {{
-        futures::join!($( $tokens )*)
+        $crate::circuits::futures::join!($( $tokens )*)
     }}
 }
