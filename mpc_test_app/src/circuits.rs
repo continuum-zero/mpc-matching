302c237ea127302fd2a7d0459113bbf498883180
teapotd <teapot404@gmail.com>
2022-02-13 11:35:02 +0100

    Proper error handling in final circuit

--- mpc_test_app/src/circuits.rs
+++ mpc_test_app/src/circuits.rs
@@ -1,4 +1,4 @@
-use std::iter;
+use std::{fmt, iter};
 
 use mpc::{
     circuits::{join_circuits_all, IntShare, WrappedShare},
@@ -6,7 +6,40 @@ use mpc::{
     ff::Field,
     MpcEngine, MpcField,
 };
+use mpc_flow::FlowError;
 use ndarray::Array2;
+
+/// Error during private matching computation.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum MatchingError<E> {
+    Execution(MpcExecutionError<E>),
+    Flow(FlowError),
+    InvalidOutput,
+    InputLengthMismatch,
+}
+
+impl<E: fmt::Display> fmt::Display for MatchingError<E> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Self::Execution(ref err) => err.fmt(f),
+            Self::Flow(ref err) => err.fmt(f),
+            Self::InvalidOutput => write!(f, "Output from circuit was invalid"),
+            Self::InputLengthMismatch => write!(f, "Parties have inconsistent input lengths"),
+        }
+    }
+}
+
+impl<E> From<MpcExecutionError<E>> for MatchingError<E> {
+    fn from(err: MpcExecutionError<E>) -> Self {
+        MatchingError::Execution(err)
+    }
+}
+
+impl<E> From<FlowError> for MatchingError<E> {
+    fn from(err: FlowError) -> Self {
+        MatchingError::Flow(err)
+    }
+}
 
 /// Vector of party preferences.
 pub type PreferenceVec = Vec<u64>;
@@ -19,7 +52,7 @@ pub async fn compute_private_matching<Engine, Error, const N: usize>(
     engine: Engine,
     preferences: PreferenceVec,
     max_preference_value: u64,
-) -> Result<usize, MpcExecutionError<Error>>
+) -> Result<usize, MatchingError<Error>>
 where
     Engine: 'static + Send + MpcEngine<Error = Error>,
     Error: 'static + Send,
@@ -28,6 +61,7 @@ where
         panic!("Protocol requires even number of parties")
     }
 
+    let num_parties = engine.num_parties();
     let party_id = engine.party_id();
 
     // Random field element that is used to hide our output from circuit.
@@ -38,14 +72,22 @@ where
         .chain(preferences.into_iter().map(Engine::Field::from))
         .collect();
 
-    let (outputs, _) = executor::run_circuit_in_background(engine, inputs, move |ctx, inputs| {
-        Box::pin(matching_circuit::<_, N>(ctx, inputs, max_preference_value))
-    })
-    .await?;
+    let (circuit_output, _) =
+        executor::run_circuit_in_background(engine, inputs, move |ctx, inputs| {
+            Box::pin(matching_circuit::<_, N>(ctx, inputs, max_preference_value))
+        })
+        .await?;
+
+    let outputs = circuit_output?;
 
     // Extract our output and "decrypt" it by subtracting mask.
-    let output = outputs[party_id] - output_mask;
-    Ok(output.truncated() as usize) // TODO: validate
+    let output = (outputs[party_id] - output_mask).truncated() as usize;
+
+    if output < num_parties {
+        Ok(output as usize)
+    } else {
+        Err(MatchingError::InvalidOutput)
+    }
 }
 
 /// Circuit used by `compute_private_matching`. Computes matching and returns masked outputs of all parties.
@@ -53,9 +95,9 @@ async fn matching_circuit<E: MpcEngine, const N: usize>(
     ctx: &MpcExecutionContext<E>,
     inputs: Vec<Vec<E::Share>>,
     max_preference_value: u64,
-) -> Vec<E::Field> {
+) -> Result<Vec<E::Field>, MatchingError<E::Error>> {
     if !inputs.iter().all(|x| x.len() == inputs[0].len()) {
-        panic!("Input length mismatch"); // TODO: don't panic
+        return Err(MatchingError::InputLengthMismatch);
     }
 
     let max_preference_value = IntShare::from_plain(ctx, max_preference_value as i64);
@@ -77,10 +119,8 @@ async fn matching_circuit<E: MpcEngine, const N: usize>(
 
     let costs = get_cost_matrix(ctx, left_preferences, right_preferences).await;
 
-    // TODO: don't unwrap
-    let (left_matches, right_matches) = mpc_flow::min_cost_bipartite_matching(ctx, costs.view())
-        .await
-        .unwrap();
+    let (left_matches, right_matches) =
+        mpc_flow::min_cost_bipartite_matching(ctx, costs.view()).await?;
 
     let matches = left_matches
         .into_iter()
@@ -92,7 +132,7 @@ async fn matching_circuit<E: MpcEngine, const N: usize>(
         .map(|(value, mask)| value.raw() + mask);
 
     ctx.ensure_integrity();
-    join_circuits_all(masked_matches.map(|x| ctx.open_unchecked(x))).await
+    Ok(join_circuits_all(masked_matches.map(|x| ctx.open_unchecked(x))).await)
 }
 
 /// Compute matrix of costs for each possible pair.
